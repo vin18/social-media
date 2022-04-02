6993ac96211bf7853ae9108bd0b3592b75b173cf
Vinit Raut <vinit.coder@gmail.com>
2022-04-02 17:31:34 +0530

    Add: Formik and yup for signin and signup forms for frontend validtion

--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import { useState } from 'react';
+import { Form, Formik } from 'formik';
+import * as yup from 'yup';
 import { Link } from 'react-router-dom';
 import { HiOutlineMail, HiOutlineKey } from 'react-icons/hi';
 import TextInput from './TextInput';
 
 const Signin = () => {
+  const initialValues = {
+    email: '',
+    password: '',
+  };
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const loginSchema = yup.object().shape({
+    email: yup
+      .string()
+      .email('Email must be a valid email')
+      .lowercase()
+      .required('Email is required'),
+    password: yup
+      .string()
+      .min(6, 'Password must be at least 6 characters long')
+      .max(50, 'Passwod must not exceed 60 characters')
+      .required('Password is required'),
+  });
+
+  const handleSubmit = (values) => {
+    console.log('Login', values);
+  };
+
   return (
     <div className="mt-16">
       <div className="w-full bg-gradient-to-r from-yellow-500 to-orange-600 h-1 max-w-md mx-auto"></div>
@@ -18,29 +44,67 @@ const Signin = () => {
           </div>
         </div>
 
-        <div className="space-y-4">
-          <TextInput
-            labelName="Email address"
-            inputType="text"
-            placeholderText="name@example.com"
-            icon={<HiOutlineMail />}
-          />
+        <Formik
+          validationSchema={loginSchema}
+          initialValues={initialValues}
+          onSubmit={handleSubmit}
+        >
+          {({
+            handleSubmit,
+            handleChange,
+            values,
+            errors,
+            setFieldValue,
+            loading,
+            isValid,
+            dirty,
+          }) => {
+            return (
+              <Form noValidate onSubmit={handleSubmit}>
+                <div className="space-y-4">
+                  <TextInput
+                    labelName="Email address"
+                    inputType="text"
+                    placeholderText="name@example.com"
+                    onChange={handleChange}
+                    icon={<HiOutlineMail />}
+                    name="email"
+                    value={values.email}
+                    error={errors.email}
+                  />
 
-          <TextInput
-            labelName="Password"
-            inputType="password"
-            placeholderText="Your password"
-            icon={<HiOutlineKey />}
-          />
+                  <TextInput
+                    labelName="Password"
+                    inputType="password"
+                    placeholderText="Your password"
+                    icon={<HiOutlineKey />}
+                    name="password"
+                    value={values.password}
+                    error={errors.password}
+                    onChange={handleChange}
+                    showPassword={showPassword}
+                    setShowPassword={setShowPassword}
+                    isPasswordInput
+                  />
 
-          <button className="bg-skin-button-accent hover:bg-skin-button-accent-hover w-full py-2 rounded-md text-skin-base mt-4 shadow">
-            Continue
-          </button>
+                  <button
+                    className={`bg-skin-button-accent hover:bg-skin-button-accent-hover w-full py-2 rounded-md text-skin-base mt-4 shadow ${
+                      (loading || !isValid || !dirty) &&
+                      'opacity-70 cursor-not-allowed'
+                    }`}
+                    disabled={loading || !isValid || !dirty}
+                  >
+                    Continue
+                  </button>
 
-          <button className="w-full py-2 rounded-md mt-4 border border-gray-300 shadow">
-            Login as guest
-          </button>
-        </div>
+                  <button className="w-full py-2 rounded-md mt-4 border-2 border-yellow-200 shadow font-bold transition duration-200 hover:scale-105">
+                    Login as guest
+                  </button>
+                </div>
+              </Form>
+            );
+          }}
+        </Formik>
       </div>
     </div>
   );
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import { useState } from 'react';
 import { HiOutlineMail, HiOutlineKey, HiOutlineUser } from 'react-icons/hi';
 import { Link } from 'react-router-dom';
+import { Form, Formik } from 'formik';
+import * as yup from 'yup';
 import TextInput from './TextInput';
 
 const Signup = () => {
+  const initialValues = {
+    email: '',
+    password: '',
+    username: '',
+    firstName: '',
+    lastName: '',
+  };
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const registerSchema = yup.object().shape({
+    email: yup
+      .string()
+      .email('Email must be a valid email')
+      .lowercase()
+      .required('Email is required'),
+    password: yup
+      .string()
+      .min(6, 'Password must be at least 6 characters long')
+      .max(50, 'Passwod must not exceed 60 characters')
+      .required('Password is required'),
+    username: yup.string().lowercase().required('Username is required'),
+    firstName: yup.string().required('First name is required'),
+    lastName: yup.string().required('Last name is required'),
+  });
+
+  const handleSubmit = (values) => {
+    console.log('Register', values);
+  };
+
   return (
     <div className="mt-16">
       <div className="w-full bg-gradient-to-r from-yellow-500 to-orange-600 h-1 max-w-md mx-auto"></div>
@@ -18,46 +50,91 @@ const Signup = () => {
           </div>
         </div>
 
-        <div className="space-y-4">
-          <TextInput
-            labelName="First name"
-            inputType="text"
-            placeholderText="Bruce"
-            icon={<HiOutlineUser />}
-          />
+        <Formik
+          validationSchema={registerSchema}
+          initialValues={initialValues}
+          onSubmit={handleSubmit}
+        >
+          {({
+            handleSubmit,
+            handleChange,
+            loading,
+            values,
+            errors,
+            setFieldValue,
+            isValid,
+            dirty,
+          }) => {
+            return (
+              <Form noValidate onSubmit={handleSubmit}>
+                <TextInput
+                  labelName="First name"
+                  inputType="text"
+                  placeholderText="Bruce"
+                  icon={<HiOutlineUser />}
+                  name="firstName"
+                  value={values.firstName}
+                  error={errors.firstName}
+                  onChange={handleChange}
+                />
 
-          <TextInput
-            labelName="Last name"
-            inputType="text"
-            placeholderText="Bruce"
-            icon={<HiOutlineUser />}
-          />
+                <TextInput
+                  labelName="Last name"
+                  inputType="text"
+                  placeholderText="Wayne"
+                  name="lastName"
+                  icon={<HiOutlineUser />}
+                  value={values.lastName}
+                  error={errors.lastName}
+                  onChange={handleChange}
+                />
 
-          <TextInput
-            labelName="Username"
-            inputType="text"
-            placeholderText="Bruce"
-            icon={<HiOutlineUser />}
-          />
+                <TextInput
+                  labelName="Username"
+                  inputType="text"
+                  placeholderText="batman"
+                  name="username"
+                  icon={<HiOutlineUser />}
+                  value={values.username}
+                  error={errors.username}
+                  onChange={handleChange}
+                />
 
-          <TextInput
-            labelName="Email address"
-            inputType="text"
-            placeholderText="name@example.com"
-            icon={<HiOutlineMail />}
-          />
+                <TextInput
+                  labelName="Email address"
+                  inputType="email"
+                  placeholderText="name@example.com"
+                  name="email"
+                  icon={<HiOutlineMail />}
+                  value={values.email}
+                  error={errors.email}
+                  onChange={handleChange}
+                />
 
-          <TextInput
-            labelName="Password"
-            inputType="password"
-            placeholderText="Your password"
-            icon={<HiOutlineKey />}
-          />
+                <TextInput
+                  labelName="Password"
+                  inputType="password"
+                  name="password"
+                  placeholderText="Your password"
+                  icon={<HiOutlineKey />}
+                  value={values.password}
+                  error={errors.password}
+                  onChange={handleChange}
+                />
 
-          <button className="bg-skin-button-accent hover:bg-skin-button-accent-hover w-full py-2 rounded-md text-skin-base mt-4 shadow">
-            Create your account
-          </button>
-        </div>
+                <button
+                  className={`bg-skin-button-accent hover:bg-skin-button-accent-hover w-full py-2 rounded-md text-skin-base mt-4 shadow ${
+                    (loading || !isValid || !dirty) &&
+                    'opacity-70 cursor-not-allowed'
+                  }`}
+                  disabled={loading || !isValid || !dirty}
+                >
+                  Create your account
+                </button>
+              </Form>
+            );
+          }}
+        </Formik>
       </div>
     </div>
   );
