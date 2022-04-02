84927037bddb2509b23b09c6c1c692f24d28c877
Vinit Raut <vinit.coder@gmail.com>
2022-04-02 20:06:01 +0530

    Add: User model

---
+++ models/userModel.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, 'First name is required'],
+    },
+    lastName: {
+      type: String,
+      required: [true, 'Last name is required'],
+    },
+    username: {
+      type: String,
+      required: [true, 'Username is required'],
+      unique: true,
+      maxlength: [30, "Username can't exceed more than 30 characters"],
+      minlength: [3, 'Username must be atleast 3 characters'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      maxlength: [60, "Password can't exceed more than 60 characters"],
+      minlength: [6, 'Password must be atleast 6 characters'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+export default User;
